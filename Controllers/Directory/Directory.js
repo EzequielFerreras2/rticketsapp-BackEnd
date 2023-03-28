40ecd6e5fb53a81120de6ad3f6c8c80363da7202
EzequielFerreras2 <ezequielferreras2@gmail.com>
2023-03-28 17:17:54 -0400

    Changes.

--- Controllers/Directory/Directory.js
+++ Controllers/Directory/Directory.js
@@ -1,27 +1,43 @@
 const express = require('express');
 const XLSX = require('xlsx');
-const fsa =require('fs');
-
-
-
-
 
 
 const readEmailsExcelBook = async(req, res = express.response) =>{
-
-    const workBook = XLSX.readFile("Controllers/Directory/EmailDiretory.xlsx");
+    const workBook = await XLSX.readFile("Controllers/Directory/EmailDiretory.xlsx");
     const workBookSheets =workBook.SheetNames;
-    console.log(workBookSheets)
     const sheet  = workBookSheets[0];
-    const data=XLSX.utils.sheet_to_json(workBook.Sheets[sheet]);
-    
-
+    const data= await XLSX.utils.sheet_to_json(workBook.Sheets[sheet]);
     return res.status(200).json({
         ok:true,
         workBookSheets: workBookSheets,
         data:data
     });
-
+}
+const readPhonesExcelBook = async(req, res = express.response) =>{
+    const workBook = await XLSX.readFile("Controllers/Directory/EmailDiretory.xlsx");
+    const workBookSheets =workBook.SheetNames;
+    const sheet  = workBookSheets[0];
+    const data= await XLSX.utils.sheet_to_json(workBook.Sheets[sheet]);
+    return res.status(200).json({
+        ok:true,
+        workBookSheets: workBookSheets,
+        data:data
+    });
 }
 
-module.exports ={readEmailsExcelBook };
+const readflotasExcelBook = async(req, res = express.response) =>{
+    const workBook = await XLSX.readFile("Controllers/Directory/Flotas.xlsx");
+    const workBookSheets =workBook.SheetNames;
+    const sheet  = workBookSheets[0];
+    const data= await XLSX.utils.sheet_to_json(workBook.Sheets[sheet]);
+    return res.status(200).json({
+        ok:true,
+        workBookSheets: workBookSheets,
+        data:data
+    });
+}
+
+
+
+
+module.exports ={readEmailsExcelBook,readflotasExcelBook };
--- routes/Directory/Directory.js
+++ routes/Directory/Directory.js
@@ -1,13 +1,13 @@
 const {Router}= require('express');
 const router =Router();
 const{jwtValidation} = require('../../middlewares/jwt-validator');
-const {readEmailsExcelBook} = require('../../Controllers/Directory/Directory')
+const {readEmailsExcelBook,readflotasExcelBook} = require('../../Controllers/Directory/Directory')
 
 router.use(jwtValidation);
 
 router.get('/emails',readEmailsExcelBook);
 // router.get('/phones',getCasesByUserId);
-// router.get('/flotas',getCasesByUserId);
+router.get('/flotas',readflotasExcelBook);
 
 
 
