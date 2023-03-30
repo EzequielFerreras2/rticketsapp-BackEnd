9b32f9bf9a5c171f67326798e8ae186d1b03befe
EzequielFerreras2 <ezequielferreras2@gmail.com>
2023-03-30 10:20:17 -0400

    Changes.

--- Controllers/Directory/Directory.js
+++ Controllers/Directory/Directory.js
@@ -3,6 +3,7 @@ const XLSX = require('xlsx');
 
 
 const readEmailsExcelBook = async(req, res = express.response) =>{
+    try {
     const workBook = await XLSX.readFile("Controllers/Directory/EmailDiretory.xlsx");
     const workBookSheets =workBook.SheetNames;
     const sheet  = workBookSheets[0];
@@ -12,20 +13,37 @@ const readEmailsExcelBook = async(req, res = express.response) =>{
         workBookSheets: workBookSheets,
         data:data
     });
-}
+    } catch (error) {
+        return res.status(500).json({
+            ok:false,
+            msg: 'Contactar con el administrador',
+            error:error
+        });
+    }
+    
+};
 const readPhonesExcelBook = async(req, res = express.response) =>{
-    const workBook = await XLSX.readFile("Controllers/Directory/Ext.xlsx");
-    const workBookSheets =workBook.SheetNames;
-    const sheet  = workBookSheets[0];
-    const data= await XLSX.utils.sheet_to_json(workBook.Sheets[sheet]);
-    return res.status(200).json({
-        ok:true,
-        workBookSheets: workBookSheets,
-        data:data
-    });
-}
+    try {
+        const workBook = await XLSX.readFile("Controllers/Directory/Ext.xlsx");
+        const workBookSheets =workBook.SheetNames;
+        const sheet  = workBookSheets[0];
+        const data= await XLSX.utils.sheet_to_json(workBook.Sheets[sheet]);
+        return res.status(200).json({
+            ok:true,
+            workBookSheets: workBookSheets,
+            data:data
+        });  
+    } catch (error) {
+        return res.status(500).json({
+            ok:false,
+            msg: 'Contactar con el administrador',
+            error:error
+        }); 
+    } 
+};
 
 const readflotasExcelBook = async(req, res = express.response) =>{
+    try {
     const workBook = await XLSX.readFile("Controllers/Directory/Flotas.xlsx");
     const workBookSheets =workBook.SheetNames;
     const sheet  = workBookSheets[0];
@@ -35,7 +53,15 @@ const readflotasExcelBook = async(req, res = express.response) =>{
         workBookSheets: workBookSheets,
         data:data
     });
-}
+        
+    } catch (error) {
+        return res.status(500).json({
+            ok:false,
+            msg: 'Contactar con el administrador',
+            error:error
+        });
+    } 
+};
 
 
 
